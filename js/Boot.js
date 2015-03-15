6859fa463d7de34a59a99093af0b490ca519d109
Sean Spearman <seanmspearman@gmail.com>
2015-03-15 08:50:50 -0700

    Allowed for possibility of player blur toggle.
    - Currently toggled with space bar.
    	* In the future, just call player.toggleBlur() wherever it should be toggled.
    	* Make sure not to toggle blur while the character is moving or bad things will happen because I suck at programming.

--- js/Boot.js
+++ js/Boot.js
@@ -21,8 +21,10 @@ Identify.Boot.prototype = {
 
 	preload: function () {
 		this.game.load.image('tile', 'images/tile.png');
-		this.game.load.spritesheet('player', 'images/lucas_walk.png', 72, 112)
-		this.game.plugins.add(new Phaser.Plugin.Isometric(this.game))
+		this.game.load.spritesheet('player', 'images/lucas_walk.png', 72, 112);
+		this.game.load.spritesheet('player_blurry', 'images/lucas_walk_blurry.png', 36, 56);
+		this.game.load.spritesheet('player_blurriest', 'images/lucas_walk_blurriest.png', 18, 28);
+		this.game.plugins.add(new Phaser.Plugin.Isometric(this.game));
 		this.game.iso.anchor.setTo(0.5, 0.5);
 	},
 
@@ -36,14 +38,52 @@ Identify.Boot.prototype = {
 				timer: 0,
 				dir: undefined
 			},
+			blur: 'blurriest',
 			move: function(dir) {
-				this.moving = { value: true, timer: 35, dir: dir}; // 35 is a magic number
-				this.sprite.animations.play(this.moving.dir);
+				if (
+					dir === 'up' && this.pos.y !== -1 ||
+					dir === 'right' && this.pos.x !== 1 ||
+					dir === 'down' && this.pos.y !== 1 ||
+					dir === 'left' && this.pos.x !== -1) {
+					this.moving = { value: true, timer: 35, dir: dir}; // 35 is a magic number
+					this.sprite.animations.play(this.moving.dir);
+				}
 			},
 			fps: 6,
 			direction: 'down',
 			addSprite: function(game) {
-				this.sprite = game.add.isoSprite(17, 17, 0, 'player', 3) // 17 is a magic number
+				if (this.sprite !== undefined) { this.sprite.destroy(); }
+				var startX = 17+this.pos.x*68; // 68 is a magic number
+				var startY = 17+this.pos.y*68; // 17 is also magic :3
+				var startFrame = 3;
+				switch(this.moving.dir) {
+					case 'up':
+						startFrame = 18;
+						break;
+					case 'right':
+						startFrame = 6;
+						break;
+					case 'down':
+						startFrame = 3;
+						break;
+					case 'left':
+						startFrame = 15;
+						break;
+				}
+				switch(this.blur) {
+					case 'none':
+						this.sprite = game.add.isoSprite(startX, startY, 0, 'player', startFrame);
+						this.sprite.scale.set(1);
+						break;
+					case 'blurry':
+						this.sprite = game.add.isoSprite(startX, startY, 0, 'player_blurry', startFrame);
+						this.sprite.scale.set(2);
+						break;
+					case 'blurriest':
+						this.sprite = game.add.isoSprite(startX, startY, 0, 'player_blurriest', startFrame);
+						this.sprite.scale.set(4);
+						break;
+				}
 				this.sprite.anchor.set(0.5);
 				this.sprite.animations.add('up', [19, 18, 20, 18], this.fps, false)
 				this.sprite.animations.add('right', [7, 6, 8, 6], this.fps, false)
@@ -54,6 +94,7 @@ Identify.Boot.prototype = {
 				var speed = 2; // 2 is a magic number
 				// If the player is not in already in the process of moving
 				if (!this.moving.value) {
+
 					if (game.cursors.up.isDown) {
 						this.move('up');
 					}
@@ -69,9 +110,9 @@ Identify.Boot.prototype = {
 				}
 				// Else the player is in the process of moving
 				else {
+					var doneMoving;
 					// If they're done moving
 					if (--this.moving.timer <= 0) {
-						//this.player.doMove()
 						switch(this.moving.dir) {
 							case 'up':
 								this.pos = { x: this.pos.x, y: this.pos.y-1 };
@@ -90,7 +131,6 @@ Identify.Boot.prototype = {
 					}
 					// Else they're not done moving
 					else {
-						//console.log('hi');
 						switch(this.moving.dir) {
 							case 'up':
 								this.sprite.isoY -= speed;
@@ -107,6 +147,12 @@ Identify.Boot.prototype = {
 						}
 					}
 				}
+			},
+			toggleBlur: function(game) {
+				if (this.blur === 'none') { this.blur = 'blurriest'; }
+				else if (this.blur === 'blurriest') { this.blur = 'blurry'; }
+				else { this.blur = 'none'; }
+				this.addSprite(game);
 			}
 		};
 		this.player.addSprite(this.game);
@@ -131,68 +177,13 @@ Identify.Boot.prototype = {
 			Phaser.Keyboard.DOWN,
 			Phaser.Keyboard.SPACEBAR
 		]);
+		this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(function() {
+			this.player.toggleBlur(this.game);
+		}, this);;
 	},
 
 	update: function() {
-		//var speed = 2; // 2 is a magic number
 		this.player.update(this.game);
-		// If the player is not in already in the process of moving
-		/*if (!this.player.moving.value) {
-			if (this.game.cursors.up.isDown) {
-				this.player.move('up');
-			}
-			else if (this.game.cursors.right.isDown) {
-				this.player.move('right');
-			}
-			else if (this.game.cursors.down.isDown) {
-				this.player.move('down');
-			}
-			else if (this.game.cursors.left.isDown) {
-				this.player.move('left');
-			}
-		}
-		// Else the player is in the process of moving
-		else {
-			// If they're done moving
-			if (--this.player.moving.timer <= 0) {
-				//this.player.doMove()
-				switch(this.player.moving.dir) {
-					case 'up':
-						this.player.pos = { x: this.player.pos.x, y: this.player.pos.y-1 };
-						break;
-					case 'right':
-						this.player.pos = { x: this.player.pos.x+1, y: this.player.pos.y };
-						break;
-					case 'down':
-						this.player.pos = { x: this.player.pos.x, y: this.player.pos.y+1 };
-						break;
-					case 'left':
-						this.player.pos = { x: this.player.pos.x-1, y: this.player.pos.y };
-						break;
-				}
-				this.player.moving.value = false;
-			}
-			// Else they're not done moving
-			else {
-				//console.log('hi');
-				switch(this.player.moving.dir) {
-					case 'up':
-						this.player.sprite.isoY -= speed;
-						break;
-					case 'right':
-						this.player.sprite.isoX += speed;
-						break;
-					case 'down':
-						this.player.sprite.isoY += speed;
-						break;
-					case 'left':
-						this.player.sprite.isoX -= speed;
-						break;
-				}
-			}
-		}
-		//console.log(this.player.sprite.isoX, this.player.sprite.isoY);
-		*/
 	},
 
 	gameResized: function (width, height) {
